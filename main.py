8b09b7b3718174ff0315638613b5ad080824cda3
morris <dltkdah777@gmail.com>
2024-08-15 15:19:32 +0900

    test : 경로 매개변수 열거형

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -17,10 +25,21 @@ async def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: str, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+def update_item(item_id: str, item: Item):
     return {"item_name": item.name, "item_id": item_id}
