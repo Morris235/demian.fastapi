a157879425fd2e69f294c8957eb235e7e355edcc
morris <dltkdah777@gmail.com>
2024-08-15 17:22:40 +0900

    test : 쿼리 매개변수

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class ModelName(str, Enum):
     lenet = "lenet"
 
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 app = FastAPI()
 
 
@@ -24,9 +26,21 @@ async def read_root():
     return {"message": "Hello World"}
 
 
+# 쿼리 매개변수
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+# 기본값을 non으로 설정하여 선택적 매개변수를 선언
 @app.get("/items/{item_id}")
-def read_item(item_id: str, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
 
 
 @app.get("/models/{model_name}")
